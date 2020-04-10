d22c6ebc964d64751aa93485950b8ab07223ab5a
Sadie Balsom <saralogsin@protonmail.com>
2020-04-10 14:02:30 +0200

    initiates project with three models

---
+++ app/models.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from app import db
+
+
+class User(db.Model):
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    date_created  = db.Column(db.DateTime,  default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime,  default=db.func.current_timestamp(),
+                                       onupdate=db.func.current_timestamp())
+
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+class Dictionary(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Dictionary {}>'.format(self.name)
--- word_routes.py
+++ word_routes.py
@@ -1,42 +1,42 @@
-from flask_restful import Resource, reqparse
-from word import Word
+# from flask_restful import Resource, reqparse
+# from word import Word
 
-parser = reqparse.RequestParser()
-parser.add_argument('name', type=str)
+# parser = reqparse.RequestParser()
+# parser.add_argument('name', type=str)
 
-class Words(Resource):
-  def get(self):
-    return [w.json() for w in Word.query.all()]
+# class Words(Resource):
+#   def get(self):
+#     return [w.json() for w in Word.query.all()]
 
-  def post(self):
-    data = parser.parse_args()
-    word = Word(data["name"])
-    word.save()
-    return word.json(), 201
+#   def post(self):
+#     data = parser.parse_args()
+#     word = Word(data["name"])
+#     word.save()
+#     return word.json(), 201
 
-class WordInstance(Resource):
-  def get(self, id):
-    word = Word.find_by_id(id)
-    if word:
-      return word.json()
-    else:
-      return {"error": "Not found"}, 404
+# class WordInstance(Resource):
+#   def get(self, id):
+#     word = Word.find_by_id(id)
+#     if word:
+#       return word.json()
+#     else:
+#       return {"error": "Not found"}, 404
 
-  def put(self, id):
-    word = Word.find_by_id(id)
-    data = parser.parse_args()
-    if word:
-      word.name = data['name']
-      word.save()
-      return word.json()
-    else:
-      return {"error": "Not found"}, 404
+#   def put(self, id):
+#     word = Word.find_by_id(id)
+#     data = parser.parse_args()
+#     if word:
+#       word.name = data['name']
+#       word.save()
+#       return word.json()
+#     else:
+#       return {"error": "Not found"}, 404
 
-  def delete(self, id):
-    word = Word.find_by_id(id)
-    if word:
-      word.destroy()
-      return 'the word has been deleted'
-    else:
-      return {"error": "Not found"}, 404
+#   def delete(self, id):
+#     word = Word.find_by_id(id)
+#     if word:
+#       word.destroy()
+#       return 'the word has been deleted'
+#     else:
+#       return {"error": "Not found"}, 404
 
