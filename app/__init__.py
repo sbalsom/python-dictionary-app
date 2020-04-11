bad3e9dcae2ccd58bf16ece6adaf17583dfdd28a
Sadie Balsom <saralogsin@protonmail.com>
2020-04-11 16:08:36 +0200

    adds login routes and authentication

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -11,5 +12,6 @@ environment_configuration = os.environ['CONFIGURATION_SETUP']
 app.config.from_object(environment_configuration)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,59 @@
-from app import app
+from app import app, db, login
+from flask_login import current_user, login_user, logout_user, login_required
+from flask import request
+from app.models import User
+from sqlalchemy.exc import IntegrityError
+
+
+@login.user_loader
+def load_user(id):
+  return User.query.get(int(id))
+
+@login.unauthorized_handler
+def unauthorized():
+    return 'Not authorized.', 401
+
+@app.route('/register', methods=['POST'])
+def register():
+  if current_user.is_authenticated:
+    return 'You are already logged in.', 200
+  username = request.json.get('username', False)
+  email = request.json.get('email', False)
+  password = request.json.get('password', False)
+  if username and email and password:
+    try:
+      # do I need to do something to avoid sql injection here ?
+      user = User(username=username, email=email)
+      user.set_password(password)
+      db.session.add(user)
+      db.session.commit()
+      return 'User {} successfully created !'.format(user.username)
+    except IntegrityError:
+      return 'User already exists'
+  return 'Please check your registration details and try again.', 422
+
+@app.route('/login', methods=['POST'])
+def login():
+  if current_user.is_authenticated:
+    return redirect(url_for('home'))
+  username = request.json['username']
+  password = request.json['password']
+  user = User.query.filter_by(username=username).first()
+  if user is None:
+    return 'User does not exist.', 401
+  elif not user.check_password(password):
+    return 'Incorrect password', 401
+  elif user.check_password(password):
+    login_user(user, remember=request.args.get('remember_me'))
+    return 'Success.', 200
+
+@app.route('/logout')
+def logout():
+  logout_user()
+  return 'Successfully logged out.', 200
 
 @app.route('/')
 @app.route('/home')
+@login_required
 def home():
-  return 'Home page'
+  return 'Welcome {}'.format(current_user.username)
