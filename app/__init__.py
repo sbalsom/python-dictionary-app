75de4c860dfe884ee076376a8a29001bd346c72b
Sadie Balsom <saralogsin@protonmail.com>
2020-04-23 14:37:06 +0200

    deployment settings updated

--- app/__init__.py
+++ app/__init__.py
@@ -2,48 +2,64 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from config import Config, DevelopmentConfig
+from config import Config
 from flask_login import LoginManager
+from logging.handlers import RotatingFileHandler
 # from . import db
 
 db = SQLAlchemy()
 migrate = Migrate()
+# do i need this login manager now that the app has tokens ?
 login = LoginManager()
 
 
-config_class = os.environ.get('CONFIG_CLASS')
+# config_class = os.environ.get('CONFIG_CLASS')
 
-def create_app(config=DevelopmentConfig):
-  print(config_class)
-  #  TODO : could pass environment as variable here
-  app = Flask(__name__)
-  app.config.from_object(config)
-  db.init_app(app)
-  migrate.init_app(app, db)
+def create_app(config=Config):
+    # print(config_class)
+    #  TODO : could pass environment as variable here
+    app = Flask(__name__)
+    app.config.from_object(config)
+    db.init_app(app)
+    migrate.init_app(app, db)
 
-  # from app.auth import bp as auth_bp
-  # app.register_blueprint(auth_bp, url_prefix='/auth')
+    # from app.auth import bp as auth_bp
+    # app.register_blueprint(auth_bp, url_prefix='/auth')
 
-  # from app.main import bp as main_bp
-  # app.register_blueprint(main_bp)
-  login.init_app(app)
+    # from app.main import bp as main_bp
+    # app.register_blueprint(main_bp)
+    login.init_app(app)
 
-  from app.api.users import bp as api_bp
-  app.register_blueprint(api_bp, url_prefix='/api')
+    from app.api.users import bp as api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
 
-  from app.api.words import bp as words_bp
-  app.register_blueprint(words_bp, url_prefix='/api')
+    from app.api.words import bp as words_bp
+    app.register_blueprint(words_bp, url_prefix='/api')
 
-  from app.api.user_words import bp as user_words_bp
-  app.register_blueprint(user_words_bp, url_prefix='/api/my')
+    from app.api.user_words import bp as user_words_bp
+    app.register_blueprint(user_words_bp, url_prefix='/api/my')
 
-  from app.api.dictionaries import bp as dictionaries_bp
-  app.register_blueprint(dictionaries_bp, url_prefix='/api/my')
+    from app.api.dictionaries import bp as dictionaries_bp
+    app.register_blueprint(dictionaries_bp, url_prefix='/api/my')
 
+    if not app.debug and not app.testing:
 
-  #  TODO : Blueprint registration ?
-  # if not app.debug and not app.testing:
-  #  TODO : Logging setup
-  return app
+        if app.config['LOG_TO_STDOUT']:
+                stream_handler = logging.StreamHandler()
+                stream_handler.setLevel(logging.INFO)
+                app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/dictionary.log', maxBytes=10240,
+                                               backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+
+            app.logger.setLevel(logging.INFO)
+            app.logger.info('Dictionary Initialization')
+    return app
 
 from app import models
--- config.py
+++ config.py
@@ -3,20 +3,19 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+  # todo: fetch all values from environment and get rid of child classes
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ENV = os.environ.get('ENV')
     FLASK_ENV = os.environ.get('FLASK_ENV')
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-    DEBUG = False
+    DEBUG = os.environ.get('DEBUG')
     TESTING = False
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 
 class ProductionConfig(Config):
     pass
-
-class DevelopmentConfig(Config):
-    DEBUG = True
 
 class TestConfig(Config):
     TESTING = True
